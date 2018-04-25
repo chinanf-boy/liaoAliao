d997f2fc9a9e4a6caaeb54a3983484af9ecfaa20
chinanf-boy <865501259@qq.com>
2018-04-25 20:07:12 +0800

    Fix publicPath

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../doc'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/liaoAliao',
 
     /**
      * Source Maps
